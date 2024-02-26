5aa24ce44c7e2e22547b8c951e9931a23738fa9e
abubakirmajidov <abubakirmajidov@gmail.com>
2024-02-26 14:27:29 +0500

    7-dars

---
+++ app.js
@@ -0,0 +1,47 @@
+// const boyi = Number(prompt("bo'yi"));
+// const vazin = Number(prompt("vazin"));
+
+// const BMI = vazin / (boyi / 100) ** 2;
+
+// if (BMI <= 18.5) {
+//   console.log("Siz ozgin ekansiz! Ko'proq oqatlaning");
+// } else if (BMI > 18.6 && BMI <= 29.9) {
+//   console.log("Siz normal holatdasiz");
+// } else {
+//   console.log("Siz semizsiz! Kam ovqatlaning yoki deyetada yuring");
+// }
+
+//////////////////////////////////////////////////////////////////////////
+
+// const numbers = [1, 3, 4, 5];
+
+// const result = Array.isArray(numbers);
+
+// console.log(result);
+
+///////////////////////////////////////////////////////////////////////////////
+
+// let arr = [1, 2, 3];
+
+// alert(arr); // 1,2,3
+// alert(String(arr) === "1,2,3");
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const result = Array.isArray(numbers);
+
+// console.log(result);
+
+// // output: true;
+
+// const names = [1, 4, 5, 1, 5, 7, 9, 10, 12, 7, 8, 9];
+
+// const newNames = [];
+
+// console.log(names.includes, i++);
+
+// for (let i = 0; i < names.length; i++) {
+//   if (names[i]) newNames.push(result);
+// }
